d9482d16c352d5a259795f893de16805dd80ae17
Nick Berejan <njberejan@gmail.com>
2016-05-12 17:37:47 -0400

    api tap open

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,11 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+#
+# from django.conf.urls import include, url
+# from django.contrib import admin
+#
+# urlpatterns = [
+#     url(r'^api/', include('trello.urls')),
+#     url(r'^admin/', admin.site.urls),
+# ]
 
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework import routers
+from trello import views
 
+router = routers.DefaultRouter()
+router.register(r'api', views.TaskViewSet)
+# router.register(r'tasks', views.TaskViewSet)
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^api/', include('trello.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
