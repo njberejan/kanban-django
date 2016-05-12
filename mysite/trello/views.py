d3d927e8490739eaeb0012e4e10fb4c3185d2e88
Nick Berejan <njberejan@gmail.com>
2016-05-12 15:57:05 -0400

    model complete

--- mysite/trello/views.py
+++ mysite/trello/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 # Create your views here.
+
 def index(request):
     return HttpResponse("Hello, world. You're at the trello index.")
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited.
+    """
+    queryset = Task.objects.all().order_by('id')
+    serializer_class = TaskSerializer
